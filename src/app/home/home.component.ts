2740b6f751c1efef8902f91acaae303f6d3a44d8
KenanKonjic <92102260+KenanKonjic@users.noreply.github.com>
2024-05-10 03:48:55 +0200

    home.component.ts updated along with package update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {UserService} from "../services/auth.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  constructor(private router:Router) {
+  constructor(private router:Router, private userService: UserService) {
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+    const username = localStorage.getItem('username').toString();
+
+    this.userService.searchUserByUsername(username)
+      .subscribe(user => {
+        if (!user.hasTakenTest) {
+          this.router.navigate(['test']);
+        }
+      });
   }
 
   navigateToHiragana(): void{
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,5 @@
 export interface UserModel {
+  hasTakenTest: boolean;
   username: string;
   password: string;
 }
